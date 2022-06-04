47b39f38e0307f76c0ed2c7c5e7da94cff1ebf28
Habsolute <85709216+Habsolute@users.noreply.github.com>
2022-06-04 14:25:50 -0400

    Request https to API whit GET methods

--- src/App.js
+++ src/App.js
@@ -1,31 +1,93 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // const dummyMovies = [
+  //   {
+  //     id: 1,
+  //     title: "Some Dummy Movie",
+  //     openingText: "This is the opening text of the movie",
+  //     releaseDate: "2021-05-18",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Some Dummy Movie 2",
+  //     openingText: "This is the second opening text of the movie",
+  //     releaseDate: "2021-05-19",
+  //   },
+  // ];
+
+  /**
+   * how to fetch api by URL whit GET method
+   */
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+    setError(null);
+    try {
+      let url = "https://swapi.dev/api/films/";
+
+      const resp = await fetch(url);
+
+      if (!resp.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const data = await resp.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.releade_date,
+        };
+      });
+      setMovieList(transformedMovies);
+      setIsLoading(false);
+      console.log(transformedMovies);
+    } catch (error) {
+      setIsLoading(false);
+      setError(error.message);
+    }
+  }
+
+  let content = <p>Found no movies.</p>;
+
+  if (movieList.length > 0) {
+    content = <MoviesList movies={movieList} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {/* whit if statement */}
+        {content}
+
+        {/* whitout if statement  */}
+        {/* {!isLoading && movieList.length > 0 && (
+          <MoviesList movies={movieList} />
+        )}
+        {!isLoading && movieList.length === 0 && !error && (
+          <p>Found no movies.</p>
+        )}
+        {!isLoading && error && <p>{error}</p>}
+        {isLoading && <p>Loading...</p>} */}
       </section>
     </React.Fragment>
   );
